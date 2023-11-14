495c42d653c282dbfc157ee0599e51160ca2d9c6
Arka Mitra <thearkamitra@gmail.com>
2023-11-14 16:26:34 +0100

    Changing batch size

---
+++ copy_events.py
@@ -0,0 +1,16 @@
+import os
+import shutil
+import glob
+
+# from the checkpoint files, copy the events.out.tfevents files to a new folder
+
+checkpoint_files = os.listdir()
+checkpoint_files = [f for f in checkpoint_files if f.startswith('checkpoints_')]
+
+new_events_dir = 'events_files'
+os.makedirs(new_events_dir, exist_ok=True)
+
+for checkpoint_file in checkpoint_files:
+    events_folder = os.path.join(checkpoint_file, 'runs-kinetics')
+    shutil.copytree(events_folder, os.path.join(new_events_dir, events_folder))
+

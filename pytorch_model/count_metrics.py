49f1ae428d9d44646baa61ea62a395a7c804d35c
Arka Mitra <thearkamitra@gmail.com>
2023-11-15 22:24:59 +0100

    Flops included

--- pytorch_model/count_metrics.py
+++ pytorch_model/count_metrics.py
@@ -2,9 +2,14 @@ import torch
 import torch.nn as nn
 from torchsummary import summary
 from model import VRC
-
+from fvcore.nn import FlopCountAnalysis
+# $ flops = FlopCountAnalysis(model, input)
+# $ flops.total()
 
 model = VRC()
 inputs = torch.randn(1, 15, 3, 224, 224)
 
 summary(model, input_size=(15, 3, 224, 224), device="cpu")
+
+flops = FlopCountAnalysis(model, inputs)
+print("The total number of FLOPs is: ", flops.total()/1e9," GFLOPs")
